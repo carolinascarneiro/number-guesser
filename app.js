2329fa4474417f4a82c173de1ccf71762b820fa8
Carolina Carneiro <cc@itacs.de>
2022-07-22 15:20:15 +0200

    adding new functions

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-let generateTarget = () => {
-    return Math.floor(Math.random() * 10);
-}
-
-const compareGuesses = (hGuess, cGuess, generateTarget) => {
+const compareGuesses = (hGuess, cGuess) => {
+    let generateTarget = () => {
+        return Math.floor(Math.random() * 10);
+    }
 
     // generate random number
-      const randomNumber = generateTarget();
-      console.log(randomNumber);
+    const randomNumber = generateTarget();
+    console.log(randomNumber);
 
-    // calculate difference between hGuess and randomNumber
-      const difHum = Math.abs(randomNumber - Number(hGuess));
+    // calculate difference between guesses and random number
+    const difHum = Math.abs(randomNumber - Number(hGuess));
+    const difComp = Math.abs(randomNumber - Number(cGuess));
 
-    // calculate difference between cGuess and randomNumber
-      const difComp = Math.abs(randomNumber - Number(cGuess));
-      difHum > difComp ? console.log('Computer won') : ('Human won');
-      difComp > difHum ? console.log('Human won') : ('Computer won');
-      if (difComp === difHum) {console.log('Human won')};
-  }
-  
-compareGuesses(1, 2)
+    if (difHum > difComp) return true;
+    else if (difComp > difHum) return false;
+    else if (difComp === difHum) return true;
+}
+
+const updateScore = (compareGuesses) => {
+
+}
+
