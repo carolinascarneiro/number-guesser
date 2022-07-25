16f403ad18e346616a1aaa4544853da108a6b18d
Carolina Carneiro <cc@itacs.de>
2022-07-25 14:58:08 +0200

    uploading solution - to be maybe corrected

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
-const compareGuesses = (hGuess, cGuess) => {
-    let generateTarget = () => {
-        return Math.floor(Math.random() * 10);
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+let generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
+
+const compareGuesses = (hGuess, cGuess, target) => {
+    const difHum = Math.abs(target - hGuess);
+    const difComp = Math.abs(target - cGuess);
+
+    if (difHum <= difComp) {
+      return true;
+    } else {
+      return false;
     }
-
-    // generate random number
-    const randomNumber = generateTarget();
-    console.log(randomNumber);
-
-    // calculate difference between guesses and random number
-    const difHum = Math.abs(randomNumber - Number(hGuess));
-    const difComp = Math.abs(randomNumber - Number(cGuess));
-
-    if (difHum > difComp) return true;
-    else if (difComp > difHum) return false;
-    else if (difComp === difHum) return true;
 }
 
-const updateScore = (compareGuesses) => {
-
+const updateScore = (s) => {
+    if (s === "human") {
+        humanScore += 1;
+    } else {
+        computerScore += 1;
+    }
 }
 
+const advanceRound = () => {
+    currentRoundNumber += 1;
+}
